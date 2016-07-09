0efaf2e9cb75d8bf618a3f7164953745d88643e2
DamianStanger <damian.stanger+bitbucket@gmail.com>
2016-07-09 22:50:26 +0100

    Added basic homecontroller test

---
+++ aspDotNetCore/test/GoServerStats.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using GoServerStats.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace GoServerStats.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void ShouldReturnIndexView()
+        {
+            var homeController = new HomeController();
+            var actionResult = (ViewResult) homeController.Index();
+            actionResult.ViewName.Should().BeNull();
+            actionResult.ViewData.Count.Should().Be(0);
+        }
+
+    }
+}
